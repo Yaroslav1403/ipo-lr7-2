03e06f66281bd3cc5b5dbf2214ebdc82e5863ef2
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-11-28 02:16:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-#Подключение модуля json
+#Импорт модуля json
 import json
 
 #Имя файла, из которого будут загружены данные
 file = 'dump.json'  
 #Просим пользователя ввести номер квалификации
-qualification_code_input = str(input("Введите номер квалификации: "))
-#Переменная, которая будет использоваться для отслеживания, была ли найдена квалификация
-skills_found = False
+input_qualification_code = str(input("Введите код квалификации: "))
+#Инициализируем переменную, которая изменится на True, если квалификация будет найдена
+found_skills = False
 
-#Открываем файл для чтения и его сод ержимое загружается в переменную content
-with open(file, 'r', encoding='utf-8') as file: 
+#Открываем файл для чтения и его содержимое загружается в переменную content
+with open(file, 'r', encoding = 'utf-8') as file: 
     content = json.load(file) 
+    
     #Проходимся по каждому элементу в content
     for skill in content:
-        #Проверяем, представляет ли собой модель "data.skill" элемент
+        #Проверяем, соответствует ли модель элемента "data.skill" 
         if skill.get("model") == "data.skill":
             #Проверяем, совпадает ли код квалификации с введённым кодом пользователя
-            if skill["fields"].get("code") == qualification_code_input: 
+            if skill["fields"].get("code") == input_qualification_code: 
                 #Сохраняем код квалификации в переменную code_of_qualification
                 code_of_qualification = skill["fields"].get("code")
                 #Сохраняем название квалификации в переменную qualification_title
-                qualification_title = skill["fields"].get("title")
-                #Переменная skills устанавливается в True
-                skills_found = True
+                title_of_qualification = skill["fields"].get("title")
+                #Сохраняем специальность в переменную specialty
+                specialty = skill["fields"].get("specialty")
+                #Переменная found_skills устанавливается в True
+                found_skills = True
             
-                #После того, как нашли код квалификации, снова проходимся по данным, чтобы найти специальность
+                #После того, как нашли код квалификации, снова проходимся по данным, чтобы найти соответствующую специальность
                 for profession in content:
-                    #Проверяем, представляет ли собой модель "data.specialty" элемент
+                    #Проверяем, соответствует ли модель элемента "data.specialty"
                     if profession.get("model") == "data.specialty":
-                        code_of_speciality = profession["fields"].get("code")
-                        #Проверяем, содержится ли код специальности в коде квалификации
-                        if code_of_speciality in qualification_code_input:  
-                            #Сохраняем 
-                            name_of_speciality = profession["fields"].get("title")
-                            speciality_educational = profession["fields"].get("c_type")
+                        code_of_specialty = profession["fields"].get("code")
+                        pk_of_specialty = profession["pk"]
+                        #Проверяем, совпадает ли идентификатор специальности с первичным ключом текущей специальности
+                        if specialty == pk_of_specialty:  
+                            #Сохраняем информацию о специальности и об образовании
+                            title_of_specialty = profession["fields"].get("title")
+                            specialty_educational = profession["fields"].get("c_type")
                             break
                 break  
 
-#Проверяем, была ли найдена квалификация.
-if not skills_found:
-   #Если квалификация не найдена, выводится сообщение "Не Найдено"
+#Проверяем, была ли найдена квалификация
+if not found_skills:
+   #Если квалификация не найдена, выводится сообщение: "Не Найдено"
    print("=============== Не Найдено ===============") 
 else:
-   #Если квалификация найдена, выводится сообщение "Найдено"
+   #Если квалификация найдена, выводится сообщение: "Найдено"
    print("=============== Найдено ===============") 
    #Вывод информации о квалификации
-   print(f"{code_of_speciality} >> Специальность {name_of_speciality} , {speciality_educational}")
-   print(f"{code_of_qualification} >> Квалификация {qualification_title}")
+   print(f"{code_of_specialty} >> Специальность: {title_of_specialty} , {specialty_educational}")
+   print(f"{code_of_qualification} >> Квалификация: {title_of_qualification}")
 
